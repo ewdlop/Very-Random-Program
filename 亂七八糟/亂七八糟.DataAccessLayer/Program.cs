52d11de254594abf59934ce1a5050c1b7f03d7c9
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2025-03-05 22:39:13 -0500

    Add DataAccessLayer console project and initial migration for Blogs and Posts

--- 亂七八糟/亂七八糟.DataAccessLayer/Program.cs
+++ 亂七八糟/亂七八糟.DataAccessLayer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Linq;
 using 亂七八糟.DataAccessLayer;
 
@@ -12,33 +15,43 @@ public class Program
     /// <param name="args"></param>
     public static async Task Main(string[] args)
     {
-        await using var db = new BloggingContext();
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddDbContext<BloggingContext>(options =>
+                    options.UseSqlite("Data Source=blogging.db"));
+            }).Build();
 
-        // Note: This sample requires the database to be created before running.
-        Console.WriteLine($"Database path: {db.DbPath}.");
+        using (var scope = host.Services.CreateScope())
+        {
+            await using var db = new BloggingContext();
+            db.Database.EnsureCreated();
 
-        // Create
-        Console.WriteLine("Inserting a new blog");
-        db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-        db.SaveChanges();
+            // Note: This sample requires the database to be created before running.
+            Console.WriteLine($"Database path: {db.DbPath}.");
 
-        // Read
-        Console.WriteLine("Querying for a blog");
-        var blog = db.Blogs
-            .OrderBy(b => b.BlogId)
-            .First();
+            // Create
+            Console.WriteLine("Inserting a new blog");
+            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            db.SaveChanges();
 
-        // Update
-        Console.WriteLine("Updating the blog and adding a post");
-        blog.Url = "https://devblogs.microsoft.com/dotnet";
-        blog.Posts.Add(
-            new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-        db.SaveChanges();
+            // Read
+            Console.WriteLine("Querying for a blog");
+            var blog = db.Blogs
+                .OrderBy(b => b.BlogId)
+                .First();
 
-        // Delete
-        Console.WriteLine("Delete the blog");
-        db.Remove(blog);
-        db.SaveChanges();
+            // Update
+            Console.WriteLine("Updating the blog and adding a post");
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(
+                new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+            db.SaveChanges();
 
+            // Delete
+            Console.WriteLine("Delete the blog");
+            db.Remove(blog);
+            db.SaveChanges();
+        }
     }
 }
